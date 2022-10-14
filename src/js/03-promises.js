17d6c87406f5ddfaa3a7c0b9a3096caa1cf7dfb2
Oleksandr P <goodwin009@gmail.com>
2022-10-14 11:59:17 +0300

    add third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,68 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
+Notify.init({ useIcon: false });
+
 const refs = {
   form: document.querySelector('.form'),
 };
 
-const fkdsd = refs.form.addEventListener('click', onClickSubmit);
+let timerId = null;
 
-function onClickSubmit(position, delay, amount) {}
+refs.form.addEventListener('click', onClickSubmit);
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    Notify.success('Сountdown starts');
-  } else {
-    // Reject
-    Notify.failure('Please choose a date in the future');
+function onClickSubmit(e) {
+  e.preventDefault();
+  if (e.target.type === 'submit') {
+    e.target.toggleAttribute('disabled');
+    const delay = refs.form.delay.value;
+    const step = refs.form.step.value;
+    const amount = refs.form.amount.value;
+    let count = 0;
+
+    //==================================================
+    setTimeout(() => {
+      timerId = setInterval(() => {
+        count += 1;
+        let isTrue = Number(amount) === Number(count);
+
+        if (isTrue) {
+          clearInterval(timerId);
+          e.target.toggleAttribute('disabled');
+        }
+        //==================================================
+        createPromise(count, step)
+          .then(({ position, delay }) => {
+            Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+        //==================================================
+      }, step);
+    }, delay);
+
+    //==================================================
   }
 }
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    // Asynchronous operation
+    const newOptions = {
+      position,
+      delay,
+    };
+    if (shouldResolve) {
+      // Fulfill
+      resolve(newOptions);
+    }
+    // Reject
+    else {
+      reject(newOptions);
+    }
+  });
+
+  return promise;
+}
